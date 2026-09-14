96e46559966e954667da3d48db05ef395570b81e
aggrxssor <markomoa@gmail.com>
2026-09-14 20:23:03 +0200

    grid sorter rework, image-modal phone gestures, ios mix-blend-mode fix

--- src/app/components/image-modal/image-modal.ts
+++ src/app/components/image-modal/image-modal.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { SiteImage } from '../../services/image.service';
 
 @Component({
@@ -7,9 +7,28 @@ import { SiteImage } from '../../services/image.service';
   templateUrl: './image-modal.html',
   styleUrl: './image-modal.css',
 })
-export class ImageModal {
+export class ImageModal implements OnChanges, OnDestroy {
   @Input() image: SiteImage | null = null;
   @Output() closeModal = new EventEmitter<void>();
+
+  private pushedState = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['image'] && this.image) {
+      if (typeof window !== 'undefined' && !this.pushedState) {
+        window.history.pushState({ modalOpen: true }, '', window.location.href);
+        this.pushedState = true;
+      }
+    }
+  }
+
+  @HostListener('window:popstate')
+  onPopState(): void {
+    if (this.pushedState) {
+      this.pushedState = false;
+      this.closeModal.emit();
+    }
+  }
 
   @HostListener('document:keydown.escape')
   onEscape(): void {
@@ -19,6 +38,18 @@ export class ImageModal {
   }
 
   close(): void {
+    if (this.pushedState) {
+      this.pushedState = false;
+      if (typeof window !== 'undefined') {
+        window.history.back();
+      }
+    }
     this.closeModal.emit();
+  }
+
+  ngOnDestroy(): void {
+    if (this.pushedState && typeof window !== 'undefined') {
+      window.history.back();
+    }
   }
 }
